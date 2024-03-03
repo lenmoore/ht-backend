f601b5608a712dd3b3160abf437636135f85fa37
helenavainmaa <helena.vainmaa@fooddocs.com>
2024-03-03 17:19:14 +0200

    https fix

--- config/env.ts
+++ config/env.ts
@@ -8,4 +8,11 @@ export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET as string;
 export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET as string;
 export const SESSION_TOKEN_SECRET = process.env.SESSION_TOKEN_SECRET as string;
 // export const TRUSTED_DOMAINS = process.env.TRUSTED_DOMAINS as string;
-export const TRUSTED_DOMAINS = ['https://192.168.8.162:5173', 'https://haihtuv.ee', 'http://localhost', 'https://localhost'];
+export const TRUSTED_DOMAINS = [
+    'https://192.168.8.162:5173',
+    'https://haihtuv.ee',
+    'http://localhost',
+    'https://localhost',
+    'http://localhost80',
+    'https://localhost80',
+];
